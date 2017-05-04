5f0dcfa875ec0a1e152b96ce2edeefafd1e27b32
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-04 18:20:10 -0400

    assigned values to variables

--- Chapter02/Examples/NameAgeAnnual0201/Vitals.java
+++ Chapter02/Examples/NameAgeAnnual0201/Vitals.java
@@ -2,5 +2,19 @@ public class Vitals {
     
     public static void main(String arg[]) {
         
+        // declare variables
+        String name;
+        int age;
+        double annualPay;
+
+        // assign values to variables
+        name = "Christopher Jack Nicholson";
+        age = 46;
+        annualPay = 125000.0;
+
+
+        // display variables
+
+
     }
 }
