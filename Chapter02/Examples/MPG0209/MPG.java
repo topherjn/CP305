f471a7c2eabbb729e981789360f0b63a2aa914c8
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-04 19:44:34 -0400

    corrected integer division into fp division

--- Chapter02/Examples/MPG0209/MPG.java
+++ Chapter02/Examples/MPG0209/MPG.java
@@ -5,6 +5,7 @@ public class MPG {
 	public static void main(String [] args) {
 
          Scanner keyboard = new Scanner(System.in);
+         double answer;
 
 		// ask (prompt) the user for number of miles driven
          System.out.println("Enter the number of miles driven: ");
@@ -19,11 +20,12 @@ public class MPG {
 		int gallons = keyboard.nextInt();
 
 
-       // calculate mpg
-       // MPG = Miles driven / Gallons of gas used
-
+        // calculate mpg
+        // MPG = Miles driven / Gallons of gas used
+        answer = (double) miles / gallons;
 
 		// display result to user
+        System.out.printf("%f\n", answer);
 
 	}
 }
