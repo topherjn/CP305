329a2ab84ba0b09186818f5de7dfb44d75cab45d
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-04 19:08:22 -0400

    Added comments as outline

--- Chapter02/Examples/MPG0209/MPG.java
+++ Chapter02/Examples/MPG0209/MPG.java
@@ -2,6 +2,17 @@ public class MPG {
 	
 	public static void main(String [] args) {
 
+		// ask (prompt) the user for number of miles driven
+		// get number of miles from user
+
+		// ask user for number of gallons used
+		// get number of gallons used from user
+
+       // calculate mpg
+       // MPG = Miles driven / Gallons of gas used
+
+
+		// display result to user
 
 	}
 }
