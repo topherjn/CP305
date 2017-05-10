5b672c7c1d1c8ca63ed25601c1af6cfa7cd51e90
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-10 19:05:24 -0400

    if else version complete

--- Chapter03/Examples/RomanNumerals.java
+++ Chapter03/Examples/RomanNumerals.java
@@ -16,11 +16,26 @@ public class RomanNumerals {
     	// get numeral from keyboard (user)
         numeral = input.nextInt();
     	
-    	// convert to Roman numeral
+    	if(numeral > 0 && numeral < 11) {
+	    	// convert to Roman numeral
+	    	if(numeral == 1) roman = "I";
+	    	else if(numeral == 2) roman = "II";
+	    	else if(numeral == 3) roman = "III";
+	    	else if(numeral == 4) roman = "IV";
+	    	else if(numeral == 5) roman = "V";
+	    	else if(numeral == 6) roman = "VI";
+	    	else if(numeral == 7) roman = "VII";
+	    	else if(numeral == 8) roman = "VIII";
+	    	else if(numeral == 9) roman = "IX";
+	    	else roman = "X";
 
-
-    	// display Roman numeral
-    	System.out.printf("%d is %s in Roman numerals.\n", numeral, roman);
+	    	// display Roman numeral
+	    	System.out.printf("%d is %s in Roman numerals.\n", numeral, roman);
+	    }
+	    else {
+	    	// yell at user
+	    	System.out.printf("%d is not between 1 and 10\n", numeral);
+	    }
     }
 
 }
