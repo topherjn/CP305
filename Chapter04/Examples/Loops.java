fa6a33f7287e795c5c676011e14f82c4b7cf1c0c
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-11 18:27:00 -0400

    sentinel while done

--- Chapter04/Examples/Loops.java
+++ Chapter04/Examples/Loops.java
@@ -1,16 +1,22 @@
+import java.util.Scanner;
+
 public class Loops {
 
      public static void main(String [] args) {
 
-          int i = 0;
-          int sum = 0;
+          int i;
+          final int SENTINEL = -1;
+          Scanner input = new Scanner(System.in);
 
-          while(i < 10) {
-               
-               sum+=i;
-               i++;
-          }
-	
-          System.out.println((double) sum / i);
+          System.out.printf("Enter a whole number: ");
+          i = input.nextInt();
+
+          while(i != SENTINEL) {
+             System.out.printf("Enter a whole number: ");
+             i = input.nextInt();
+	  }
+
+          System.out.println("Loop done");
+         
      }
 }
