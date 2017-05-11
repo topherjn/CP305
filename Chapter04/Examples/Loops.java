97bd8fe701c465ae72187dab4692adede6aeb396
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-11 18:00:19 -0400

    very basic while loop

---
+++ Chapter04/Examples/Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+
+     public static void main(String [] args) {
+
+          int i = 0;
+
+          while(i < 10) {
+               System.out.println("Chris");
+
+               i++;
+          }
+	
+          System.out.println("Loop done");
+     }
+}
