9f9be742cfa7c3ef3d08683f306ca2bdac0259c0
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-11 17:49:39 -0400

    String equals example

---
+++ Chapter03/Examples/StringExample.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class StringExample {
+
+    public static void main(String [] args) {
+
+    	String word1 = new String("apple");
+        String word2 = new String("apple");
+        
+        if(word1 == word2)  System.out.println("Equal");
+    }
+}
