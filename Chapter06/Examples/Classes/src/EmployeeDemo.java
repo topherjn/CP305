8ad8af4f2640e2f5da1e73895b5acc26a695e254
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-24 19:21:29 -0400

    Employee class more or less complete

--- Chapter06/Examples/Classes/src/EmployeeDemo.java
+++ Chapter06/Examples/Classes/src/EmployeeDemo.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by nicholson on 5/24/2017.
  */
 public class EmployeeDemo {
 
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        Employee emp1 = new Employee("John Smith", 111, "Finance", "CFO");
+        Employee emp2 = new Employee("Susan Meyers", 47899, "Accounting", "VP" );
+        Employee emp3 = new Employee(input.nextLine(), input.nextInt(), input.nextLine(),input.nextLine());
+
+        System.out.println(emp2.getName());
+
+        emp2.setName("TAFKAP");
+
+        System.out.println(emp2.getName());
+
+      
+
+
+
+
     }
 }
